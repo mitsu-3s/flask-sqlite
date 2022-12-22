4386663274987d4b349b226afb764518b3a45112
mitsu-3s <mitsuuu.s3s@gmail.com>
2022-12-22 10:30:18 +0900

    create main system

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///Memo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_ECHO']=True
+
+db = SQLAlchemy(app)
+
+class Memo(db.Model):
+    __tablename__ = 'Test'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+@app.before_first_request
+def init():
+    db.create_all()
+
+@app.route('/', methods=['GET'])
+def index():
+    datas = Memo.query.all()
+    return render_template('index.html', memos = datas)
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == "POST":
+        text = request.form.get('text')
+        author = request.form.get('author')
+        # BlogArticleのインスタンスを作成
+        memo = Memo(text=text, author=author)
+        db.session.add(memo)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('create.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
